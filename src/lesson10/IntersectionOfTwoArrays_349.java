1f8a8b185578203f60b09906e40ab82174de7d5b
nguyenntt <106979668+Nolongeruser9771@users.noreply.github.com>
2023-02-08 02:18:48 +0700

    add exercise

--- src/lesson10/IntersectionOfTwoArrays_349.java
+++ src/lesson10/IntersectionOfTwoArrays_349.java
@@ -1,5 +1,37 @@
 package lesson10;
 
-public class IntersectionOfTwoArrays_349 {
+import java.util.*;
 
+public class IntersectionOfTwoArrays_349 {
+    public static void main(String[] args) {
+        int[] n1 = {1,2,2,1};
+        int[] n2 = {2,2};
+        intersection(n1,n2);
+    }
+
+    public static int[] intersection(int[] n1, int[] n2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> interSet = new HashSet<>();
+
+        //Ý tưởng: tạo set1 với key là giá trị không trùng lặp của n1;
+        for (int i:n1) {
+            if (!set1.contains(i)) set1.add(i);
+        }
+
+        //Tạo interSet chứa giá trị trùng lặp của set1 và n2
+        for (int n: n2) {
+            if (set1.contains(n)) {
+                interSet.add(n);
+            }
+        }
+
+        //convert set to array
+        int[] intersection = new int[interSet.size()];
+        int i = 0;
+        for (int n:interSet) {
+            intersection[i] = n;
+            i++;
+        }
+        return intersection;
+    }
 }
